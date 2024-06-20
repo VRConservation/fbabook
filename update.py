bb526d9e6df55b7fae70752074cd077ade3d334c
VRConservation <vance.russell@outlook.com>
2024-06-20 12:33:53 +0100

    deleted program2

--- update.py
+++ update.py
@@ -12,5 +12,5 @@ subprocess.run(["jb", "build", "--all", "."], check=True)
 
 # Push and sync the repository to GitHub make sure to change the 'Update repo' message
 subprocess.run(["git", "add", "."], check=True)
-subprocess.run(["git", "commit", "-m", "project chp edits header not showing up"], check=True)
+subprocess.run(["git", "commit", "-m", "deleted program2"], check=True)
 subprocess.run(["git", "push"], check=True)
