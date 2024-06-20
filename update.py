542a6e9b2f5236c1896df28cef1de86321c2690a
VRConservation <vance.russell@outlook.com>
2024-06-20 12:29:48 +0100

    project chp edits header not showing up

--- update.py
+++ update.py
@@ -5,12 +5,12 @@ import subprocess
 orig_dir = os.getcwd()
 
 # run the ghp-import -n -p -f _build/html
-subprocess.run(["ghp-import", "-n", "-p", "-f", "_build/html"], check=True)
+# subprocess.run(["ghp-import", "-n", "-p", "-f", "_build/html"], check=True)
 
 # Run the command jb build --all . after the loop
 subprocess.run(["jb", "build", "--all", "."], check=True)
 
 # Push and sync the repository to GitHub make sure to change the 'Update repo' message
 subprocess.run(["git", "add", "."], check=True)
-subprocess.run(["git", "commit", "-m", "project chp edits"], check=True)
+subprocess.run(["git", "commit", "-m", "project chp edits header not showing up"], check=True)
 subprocess.run(["git", "push"], check=True)
