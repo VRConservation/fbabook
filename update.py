9a23160da98cc061768adc0886fe70afb0100107
VRConservation <vance.russell@outlook.com>
2024-08-15 09:49:20 +0100

    monitoring rewrite

--- update.py
+++ update.py
@@ -12,5 +12,5 @@ subprocess.run(["jb", "build", "--all", "."], check=True)
 
 # Push and sync the repository to GitHub make sure to change the 'Update repo' message
 subprocess.run(["git", "add", "."], check=True)
-subprocess.run(["git", "commit", "-m", "fixed build not happening"], check=True)
+subprocess.run(["git", "commit", "-m", "monitoring rewrite"], check=True)
 subprocess.run(["git", "push"], check=True)
