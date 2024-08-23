be3c97e8506de7706eaf98c165e2af87161b4239
VRConservation <vance.russell@outlook.com>
2024-08-23 09:39:51 +0100

    ecivis chapter

--- update.py
+++ update.py
@@ -12,5 +12,5 @@ subprocess.run(["jb", "build", "--all", "."], check=True)
 
 # Push and sync the repository to GitHub make sure to change the 'Update repo' message
 subprocess.run(["git", "add", "."], check=True)
-subprocess.run(["git", "commit", "-m", "added cover page"], check=True)
+subprocess.run(["git", "commit", "-m", "ecivis chapter"], check=True)
 subprocess.run(["git", "push"], check=True)
