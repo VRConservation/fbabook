795f9e77fff9b6c3400e5e8d182eae36fd018dcc
VRConservation <vance.russell@outlook.com>
2024-09-14 10:28:16 +0100

    russell citation added

--- update.py
+++ update.py
@@ -12,5 +12,5 @@ subprocess.run(["jb", "build", "--all", "."], check=True)
 
 # Push and sync the repository to GitHub make sure to change the 'Update repo' message
 subprocess.run(["git", "add", "."], check=True)
-subprocess.run(["git", "commit", "-m", "monitoring chapter"], check=True)
+subprocess.run(["git", "commit", "-m", "russell citation added"], check=True)
 subprocess.run(["git", "push"], check=True)
