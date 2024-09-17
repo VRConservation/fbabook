8d7dd8a14eff10fdcfc57babdd465183f2bea5d4
VRConservation <vance.russell@outlook.com>
2024-09-17 10:05:07 +0100

    spell check

--- update.py
+++ update.py
@@ -12,5 +12,5 @@ subprocess.run(["jb", "build", "--all", "."], check=True)
 
 # Push and sync the repository to GitHub make sure to change the 'Update repo' message
 subprocess.run(["git", "add", "."], check=True)
-subprocess.run(["git", "commit", "-m", "adding resilience chapter from New Leaf"], check=True)
+subprocess.run(["git", "commit", "-m", "spell check"], check=True)
 subprocess.run(["git", "push"], check=True)
