15583e9c5e8aecaf9cb2876a52d3aed417e740dd
VRConservation <vance.russell@outlook.com>
2024-09-24 11:07:28 +0100

    fixing resilience cites

--- update.py
+++ update.py
@@ -14,3 +14,17 @@ subprocess.run(["jb", "build", "--all", "."], check=True)
 subprocess.run(["git", "add", "."], check=True)
 subprocess.run(["git", "commit", "-m", "fixing resilience cites"], check=True)
 subprocess.run(["git", "push"], check=True)
+
+# Open local index file in web browser
+# Define the path to the intro.html file
+file_path = r'file://D:\OneDrive\1_Consulting\CAL FIRE Biz Devo\CAL FIRE Biz Devo Shared\Communications\fbabook\_build\html\index.html'
+
+# Define the command to open the file in Brave or Chrome
+# For Brave
+command_brave = f'start brave "{file_path}"'
+
+# For Chrome
+# command_chrome = f'start chrome "{file_path}"'
+
+# Execute the command in Brave. Use command_chrome for Chrome
+subprocess.run(command_brave, shell=True)  # Use command_chrome for Chrome
